70079e96d68e348a6de40256e13e99d2e0b3ffec
c-cardie <ccardie001@student.franciscan.edu>
2024-12-05 18:06:48 -0500

    added some comments

--- TestNWord.asm
+++ TestNWord.asm
@@ -1,3 +1,5 @@
+;Author: Claire Cardie
+
 ;first call
 ;load Q = 0x03C3
 LDA #$C3
@@ -36,21 +38,31 @@ LDY #$00
 
 JSR SubNWord
 
+;store pointer in zero-page
 LDA $4000
 STA $04
 LDA $4001
 STA $05
 
+;reset offset for zero-page
 LDY #$00
 
 dereference:
 
+;dereference pointer
+;3000 is stored in $0004 and $0005
+;($04) references two consecutive memory locations
+;because we are referencing a two byte address
+;load the contents of $(3000 + Y) on the A register
 LDA ($04), Y
 
+;store in $(5000 + Y)
+;stores subtraction result in litte endian format
 STA $5000, Y
 
 INY
 
+;have we dereferenced all N bytes?
 CPY $01FB
 BNE dereference
 
@@ -92,17 +104,25 @@ LDY #$00
 
 JSR SubNWord
 
+;don't need to store pointer in zero page here
+;because we are using the same pointer as the last call
+
 ;reset Y
 LDY #$00
 
 dereference2:
 
+;peeking at same location in zero page
+;still dereferencing address $3000
 LDA ($04), Y
 
+;store dereferenced pointer in a different location
+;so we can see answers for each subtraction
 STA $5010, Y
 
 INY
 
+;have we dereferenced all N bytes?
 CPY $01FB
 BNE dereference2
 
@@ -187,10 +207,17 @@ STA $02
 LDA $0107, X
 STA $03
 
+;set carry bit to 1
 SEC
+;push the status register onto the stack
 PHP
+
 loop:
 
+;restore status register
+;do after doing CPY
+;compare instructions mess with the status bits
+;if not restored, you will get off by one errors in subtraction results
 PLP
 
 ;put Q's current byte on A register
@@ -207,6 +234,8 @@ STA $3000, Y
 ;increment Y before comparing
 INY
 
+;push status register onto the stack
+;so we can restore after CPY
 PHP
 
 ;have we gone through N bytes?
@@ -214,6 +243,7 @@ CPY $01FB
 ;if not, repeat subtraction on next byte
 BNE loop
 
+;restore status register even after exiting loop
 PLP
 
 ;returning address where subtraction result is stored
@@ -224,7 +254,7 @@ STA $0104, X
 LDA #$00
 STA $0103, X
 
-;retrieve and store address on the stack
+;retrieve from stack and store address in memory
 LDA $0103, X
 STA $4000
 LDA $0104, X
